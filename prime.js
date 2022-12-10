690c29010207a344507a1e16b63106463f015c36
p20aswini <pattaaswini79@gmail.com>
2022-12-10 23:28:03 +0530

    successfully created

--- prime.js
+++ prime.js
@@ -1,9 +1,18 @@
 function CheckPrime(num){
+    let count=0;
     for(let i=1;i<=num;i++){
-        if(num%i){
-            return true;
+        if(num%i==0){
+            count++;
         }
+    }
+    if(count==2){
+        return true;
     }
     return false;
 }
-CheckPrime(13);
+ let ans=CheckPrime(13);
+ if(ans==true){
+ console.log("prime");
+ }else{
+    console.log("not prime");
+ }
